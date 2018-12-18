f8a46cad1b2f71bf8a2731fff44a04dc8e39a3ac
Athan Clark <athan.clark@gmail.com>
2018-12-18 11:08:37 -0700

    starting strict implementations

--- src/Data/ByteString/Z85.hs
+++ src/Data/ByteString/Z85.hs
@@ -5,8 +5,10 @@
 module Data.ByteString.Z85 where
 
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
 
 import Data.Text (Text)
+import qualified Data.Text as T
 
 
 data EncodeError
@@ -14,12 +16,16 @@ data EncodeError
 
 
 encode :: ByteString -> Either EncodeError Text
-encode _ = Left BSMod4
+encode bs
+  | BS.length bs `mod` 4 /= 0 = Left BSMod4
+  | otherwise = Left BSMod4
 
 
 data DecodeError
-  = TestMod5
+  = TextMod5
 
 
 decode :: Text -> Either DecodeError ByteString
-decode _ = Left TestMod5
+decode t
+  | T.length t `mod` 5 /= 0 = Left TextMod5
+  | otherwise = Left TextMod5
